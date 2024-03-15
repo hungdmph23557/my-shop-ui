1573e9c326417bfcb273441df138e397b31d0b73
MinhGRX <tuanminh85lvt@gmail.com>
2024-03-15 18:20:48 +0700

    update login.component

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  public username: any;
+  public password: any;
+  public selectedValues: any;
 }
