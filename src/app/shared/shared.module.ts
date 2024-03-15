77c1e928ba3ffa6cce7b7c081103e12dd9355a6c
MinhGRX <tuanminh85lvt@gmail.com>
2024-03-15 18:20:23 +0700

    update shared.module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { ImageModule } from 'primeng/image';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    DropdownModule,
+    FormsModule,
+    ImageModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    CheckboxModule
   ]
 })
 export class SharedModule { }
